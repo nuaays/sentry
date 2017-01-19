27f4e503b2db5e52b1745dc215cd22ca3a16c758
Katie Lundsgaard <katie@getsentry.com>
2017-01-18 17:20:31 -0800

    Break out commit data retrieval into separate method

--- src/sentry/api/serializers/models/release.py
+++ src/sentry/api/serializers/models/release.py
@@ -10,49 +10,7 @@ from sentry.models import Release, ReleaseCommit, ReleaseProject, TagValue, User
 
 @register(Release)
 class ReleaseSerializer(Serializer):
-    # O(a b c)
-
-    # for every release
-    #      for every release commit
-    #            for every author
-    # def _get_commit_authors(self, item_list, user):
-    #     # TODO: feature switch
-
-    #     # for each release in item_list
-    #     # do a subquery and get their authors
-    #     # add authors to that item_list entry
-    #     releasecommits = ReleaseCommit.objects.filter(
-    #         release=item
-    #     ).select_related("commit")
-    #     authors = []
-    #     for releasecommit in releasecommits:
-    #         author = releasecommit.commit.author
-    #         try:
-    #             author = User.objects.get(email=author.email)
-    #         except MultipleObjectsReturned:
-    #             author = User.objects.filter(email=author.email).first()
-    #         except ObjectDoesNotExist:
-    #             pass
-    #         authors.append(serialize(author))
-
-    #     return authors
-
-    def get_attrs(self, item_list, user):
-        tags = {
-            tk.value: tk
-            for tk in TagValue.objects.filter(
-                project_id__in=ReleaseProject.objects.filter(
-                    release__in=item_list
-                ).values_list('project_id', flat=True),
-                key='sentry:release',
-                value__in=[o.version for o in item_list],
-            )
-        }
-        owners = {
-            d['id']: d
-            for d in serialize(set(i.owner for i in item_list if i.owner_id), user)
-        }
-
+    def _get_commit_metadata(self, item_list, user):
         release_commits = list(ReleaseCommit.objects.filter(
             release__in=item_list).prefetch_related("commit__author"))
 
@@ -61,6 +19,7 @@ class ReleaseSerializer(Serializer):
 
         author_emails = set(rc.commit.author.email for rc in release_commits)
 
+        # TODO: Consider UserEmail models, organization filter
         # NOTE: Possible to return multiple User objects for a single email
         users = list(User.objects.filter(email__in=author_emails))
         users_by_email = {}
@@ -91,23 +50,35 @@ class ReleaseSerializer(Serializer):
                 'commit_count': commit_count_by_release_id[item.id],
                 'author_count': 0,
                 'authors': authors_by_release_id[item.id].values(),
+            }
+        return result
+
+    def get_attrs(self, item_list, user):
+        tags = {
+            tk.value: tk
+            for tk in TagValue.objects.filter(
+                project_id__in=ReleaseProject.objects.filter(
+                    release__in=item_list
+                ).values_list('project_id', flat=True),
+                key='sentry:release',
+                value__in=[o.version for o in item_list],
+            )
+        }
+        owners = {
+            d['id']: d
+            for d in serialize(set(i.owner for i in item_list if i.owner_id), user)
+        }
+
+        release_metadata_attrs = self._get_commit_metadata(item_list, user)
+
+        result = {}
+        for item in item_list:
+            result[item] = {
                 'tag': tags.get(item.version),
                 'owner': owners[six.text_type(item.owner_id)] if item.owner_id else None,
             }
+            result[item].update(release_metadata_attrs[item])
 
-        # if features.has('organizations:release-commits', actor=user):
-        # numCommits
-        # get number of subcommits
-        # num_commits = ReleaseCommit.objects.count(release_id=release.id)
-        # for item in item_list:
-        # authors = self._get_commit_authors(item_list, user)
-        # result[item] = {
-        #     'authors': authors,
-        #     'author_count': len(authors),
-        #     'commit_count': ReleaseCommit.objects.filter(release=item).count(),
-        #     'tag': tags.get(item.version),
-        #     'owner': owners[six.text_type(item.owner_id)] if item.owner_id else None,
-        # }
         return result
 
     def serialize(self, obj, attrs, user):
